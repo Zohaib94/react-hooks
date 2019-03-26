a800fd9165a6306178e8af6f7d35c3ea351a00cb
Zohaib Ahmed <zohaibahmedbutt@gmail.com>
2019-03-26 19:07:51 +0500

    Section 2 Lecture 2: useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        App
+const App = () => {
+  const [userQuery, setUserQuery] = useState("");
+
+  const updateUserQuery = event => {
+    setUserQuery(event.target.value);
+  };
+
+  const searchQuery = () => {
+    window.open(`https://www.google.com/search?q=${userQuery}`, "_blank");
+  };
+
+  const handleKeypress = event => {
+    if (event.target === "Enter") {
+      searchQuery();
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello Mr Butt</h1>
+      <div className="form">
+        <input
+          value={userQuery}
+          onChange={updateUserQuery}
+          onKeyDown={handleKeypress}
+        />
+        <button onClick={searchQuery}>Search</button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
