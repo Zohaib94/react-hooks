5242bfaec44d3e5f9b28751d33044313acde930b
Zohaib Ahmed <zohaibahmedbutt@gmail.com>
2019-03-31 19:09:25 +0500

    Section 6 Lecture 15: CreateReaction functionality

--- src/createReaction.js
+++ src/createReaction.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { REACTION_OBJECTS } from "./state/types";
+import { createReaction } from './state/actions';
+import { useAppContext } from './Hooks';
 
-function createReaction() {
+function CreateReaction(props) {
+  const { state: { username }, pubsub: { publish } } = useAppContext();
+  const messageId = props.messageId;
+
+  const publishReaction = ({type, emoji}) => () => {
+    publish(createReaction({ type, emoji, username, messageId }))
+  }
+
   return (
     <div className="CreateReaction">
       {REACTION_OBJECTS.map(REACTION_OBJECT => {
         const { type, emoji } = REACTION_OBJECT;
 
-        return <span key={type}>{emoji}</span>;
+        return <span key={type} onClick={publishReaction({type, emoji})}>{emoji}</span>;
       })}
     </div>
   );
 }
 
-export default createReaction;
+export default CreateReaction;
